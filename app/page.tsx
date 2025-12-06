c6353d2c023ea8c2d609b4fca636c4db92c844d4
onursel42-collab <onursel42@gmail.com>
2025-12-06 12:01:39 +0300

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function HomePage() {
-  return (
-    <div style={{ padding: 40, fontSize: 22 }}>
-      <h1>TradePig Global Çalışıyor 🐷🚀</h1>
-      <p>Supabase + Vercel bağlantısı başarıyla aktif!</p>
-    </div>
-  );
-}
