61672c65b46e7a015c49ff47d4d26a8c80c4a2d0
onursel42-collab <onursel42@gmail.com>
2025-12-06 10:18:49 +0300

    Add metadata and RootLayout component

--- app/layout.js
+++ app/layout.js
@@ -1,1 +1,13 @@
 
+export const metadata = {
+  title: "TradePi Global",
+  description: "B2B Marketplace",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="tr">
+      <body>{children}</body>
+    </html>
+  );
+}
