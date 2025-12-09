a53d4f1e789560b96bd26629fb7b4357303b613c
onursel42-collab <onursel42@gmail.com>
2025-12-10 00:53:18 +0300

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>TradePiGlobal Yeni Nesil B2B</h1>
+      <p>Supabase + Next.js altyapısı aktif 🔥</p>
+    </div>
+  );
+}
