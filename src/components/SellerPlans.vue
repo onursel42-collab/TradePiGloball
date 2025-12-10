375e0e90479ab7c3c44a6cf53755207c1e913605
onursel42-collab <onursel42@gmail.com>
2025-12-11 01:03:24 +0300

    Create SellerPlans.vue

---
+++ src/components/SellerPlans.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Satıcı Üyelik Paketleri</h2>
+    <p>Burası geçici SellerPlans alanı.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SellerPlans',
+};
+</script>
+
+<style scoped>
+</style>
