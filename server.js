386f8519ddf886c1ee2e5c7bb7b8baa675525eea
onursel42-collab <onursel42@gmail.com>
2025-12-10 22:10:14 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = process.env.PORT || 3000;
 
+const port = process.env.PORT || 10000;
+
+// dist klasörünü statik sun
 app.use(express.static(path.join(__dirname, 'dist')));
 
+// SPA fallback
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
