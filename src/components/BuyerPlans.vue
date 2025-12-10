fd91535d4a0c1498cbc78d8c15e3b0ab50a806e2
onursel42-collab <onursel42@gmail.com>
2025-12-11 01:09:26 +0300

    Create BuyerPlans.vue

---
+++ src/components/BuyerPlans.vue
@@ -0,0 +1,18 @@
+<template>
+  <section class="buyer-plans">
+    <h2>Alıcı Üyelik Paketleri</h2>
+    <p>Alıcı tarafı için paketler burada listelenecek.</p>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'BuyerPlans'
+};
+</script>
+
+<style scoped>
+.buyer-plans {
+  padding: 2rem 1rem;
+}
+</style>
