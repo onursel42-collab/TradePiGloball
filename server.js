8ef54ec7016630f3d714beeed17127e32a9d4284
onursel42-collab <onursel42@gmail.com>
2025-12-11 23:01:30 +0300

    Update server.js

--- server.js
+++ server.js
@@ -3,19 +3,36 @@ import cors from "cors";
 import dotenv from "dotenv";
 dotenv.config();
 
-// src klasöründeki supabaseClient
 import { supabase } from "./src/config/supabaseClient.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-// Sağlık kontrolü (test)
+// Test endpoint
 app.get("/", (req, res) => {
-  res.send("TradePiGlobal Backend Çalışıyor ✔️");
+  res.send("TradePiGloball Backend Çalışıyor ✔️");
 });
 
-// İleride buraya /api/... endpointlerini ekleyeceğiz
+// Üyelik Planları API
+app.get("/plans", async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from("plans")
+      .select("*")
+      .order("price");
+
+    if (error) {
+      console.error("Supabase Hatası:", error);
+      return res.status(500).json({ error: "Supabase error", details: error });
+    }
+
+    return res.json(data);
+  } catch (err) {
+    console.error("Server Error:", err);
+    res.status(500).json({ error: "Server error", details: err });
+  }
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => {
