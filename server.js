8620723d5af7407b56d1e6f526009340e00c8ebf
onursel42-collab <onursel42@gmail.com>
2025-12-11 23:55:05 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -9,7 +10,7 @@ dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
-
+app.use(express.static("public"));
 // Sağlık kontrolü (test)
 app.get("/", (req, res) => {
   res.send("TradePiGloball Backend Çalışıyor ✔");
