082b67b2f14265d70b225b0af4ea143b7b81a88f
onursel42-collab <onursel42@gmail.com>
2025-12-13 15:01:06 +0300

    Delete README.mdsrc/modules/webhooks directory

--- README.mdsrc/modules/webhooks/webhooks.routes.js
+++
@@ -1,6 +0,0 @@
-import { Router } from "express";
-import { escrowComWebhookHandler } from "./escrowcom.webhook.js";
-
-export const webhooksRouter = Router();
-
-webhooksRouter.post("/escrowcom", escrowComWebhookHandler);
