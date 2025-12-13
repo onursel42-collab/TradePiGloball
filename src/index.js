951b1bd2c6cdc24202147f58f916c27d4ebaec3c
onursel42-collab <onursel42@gmail.com>
2025-12-13 19:11:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import { escrowWebhookRouter } from './escrow/webhook.js';
 
 const app = express();
 app.use(express.json());
-
+app.get("/validation-key.txt", (req, res) => {
+  res.setHeader("Content-Type", "text/plain; charset=utf-8");
+  res.status(200).send(process.env.PI_VALIDATION_KEY);
+});
 // Escrow webhook
 app.use('/webhooks/escrow', escrowWebhookRouter);
 
